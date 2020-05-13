bbdf151ec02124891d33fe40ae435d0b4a92caed
Martin Hertig <martin.hertig@gmail.com>
2020-05-13 12:58:17 +0200

    add color changer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@
 
 import "./css/main.css";
 
+var colors = ["pink","#52A158","#FF887D","#FFD6AC","#AEA7FA"];
+var colorsIndex = 0;
+
+function getNextColor(startIndex) {
+  if(colorsIndex >= colors.length - 1) {
+    colorsIndex = 0;
+  } else {
+    colorsIndex++;
+  }
+  return colors[colorsIndex];
+};
 
 var slider = tns({
   container: '.slider',
@@ -9,3 +20,15 @@ var slider = tns({
   autoplay: false,
   controlsContainer: ".prevNext"
 });
+
+var customizedFunction = function (info, eventName) {
+  // direct access to info object
+  console.log(info.event.type, info.container.id);
+  var color = getNextColor();
+  console.log(color);
+  document.documentElement.style
+    .setProperty('--primary', color);
+}
+
+// bind function to event
+slider.events.on('transitionStart', customizedFunction);
